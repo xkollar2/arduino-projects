ab628a480901b42f25e2d68a3425b05afde476db
Denis Kollar <kollar.den@gmail.com>
2022-11-04 01:54:55 +0100

    Added project02

---
+++ ArduinoProjectsBook/Task02/task02/task02.ino
@@ -0,0 +1,30 @@
+int switchState = 0;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(2, INPUT);
+
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  switchState = digitalRead(2);
+  // Serial.println("read: " + String(switchState));
+  if (switchState == LOW) {
+    digitalWrite(3, HIGH);
+    digitalWrite(4, LOW);
+    digitalWrite(5, LOW);
+  } else {
+    digitalWrite(3, LOW);
+    digitalWrite(4, LOW);
+    digitalWrite(5, HIGH);
+
+    delay(250);
+    digitalWrite(4, HIGH);
+    digitalWrite(5, LOW);
+    delay(250);
+  }
+}
